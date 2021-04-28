99750f2ffed28be6f0e8d2b1ed8f47899d6ef18d
luthfy <luthfy.megumi@gmail.com>
2021-04-29 01:39:12 +0800

    patch text tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -541,7 +541,7 @@
                                     <a href="{{url('post/' . $item->slug)}}">{{$item->title_post}}</a>
                                 </h4>
                                 <p style="text-align: justify;">
-                                    {!! substr($item->content_post, 0, 200) !!}
+                                    {!! Str::limit(strip_tags($item->content_post), 200, '...') !!}
                                 </p>
                             </div>
                             <span>
